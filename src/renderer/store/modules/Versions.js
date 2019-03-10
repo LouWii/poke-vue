04ef75e6bff8909dfff8aaf0ab622657339383a8
Louwii <link14.online@gmail.com>
2019-03-09 20:14:43 -0800

    add Version Vuex Module

---
+++ src/renderer/store/modules/Versions.js
@@ -0,0 +1,18 @@
+const getInitialState = function () {
+  return { }
+}
+
+const state = getInitialState()
+
+const getters = {}
+
+const actions = {}
+
+const mutations = {}
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+}
