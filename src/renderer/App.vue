bcf060fe8891d23ca9a0fd14e2e461fb8edf6bde
Louwii <link14.online@gmail.com>
2019-03-15 23:55:17 -0700

    add custom font

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -23,17 +23,52 @@
 </script>
 
 <style lang="scss">
+  @font-face {
+    font-family: 'Muli', sans-serif;
+    src: url('/static/fonts/Muli-Regular.ttf');
+    font-weight: 400;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'Muli', sans-serif;
+    src: url('/static/fonts/Muli-Italic.ttf');
+    font-weight: 400;
+    font-style: italic;
+  }
+  @font-face {
+    font-family: 'Muli', sans-serif;
+    src: url('/static/fonts/Muli-Light.ttf');
+    font-weight: 300;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'Muli', sans-serif;
+    src: url('/static/fonts/Muli-SemiBold.ttf');
+    font-weight: 600;
+    font-style: normal;
+  }
+
   *, ::after, ::before {
     box-sizing: border-box;
   }
 
   body {
+    font-family: 'Muli', sans-serif;
+    font-weight: 400;
     margin: 0;
     padding: 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 
   dl, ol, ul {
     margin-top: 0;
     margin-bottom: 1rem;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    cursor: default;
+    font-weight: 600;
+    user-select: none;
   }
 </style>
